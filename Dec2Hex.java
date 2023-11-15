a19344d52e44df7da9f706295b9c109973c215b8
MOEZR1 <mohammedalbahly206@gmail.com>
2023-11-15 19:33:46 +0000

    made changes to all files

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,31 +1,47 @@
 public class Dec2Hex {
- 
+
+    /**
+     * Converts a decimal number to its hexadecimal representation.
+     * @param decimalNumber The decimal number to convert.
+     * @return The hexadecimal representation of the given decimal number.
+     */
     public static String convertToHex(int decimalNumber) {
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        if (decimalNumber == 0) {
+            return "0"; // Handle the edge case for 0
+        }
+
+        char[] hexChars = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
         StringBuilder hexadecimal = new StringBuilder();
+
         while (decimalNumber > 0) {
             int rem = decimalNumber % 16;
-            hexadecimal.insert(0, ch[rem]);
+            hexadecimal.insert(0, hexChars[rem]);
             decimalNumber /= 16;
         }
+
         return hexadecimal.toString();
     }
- 
-public static void main(String[] args) {
-    if (args.length == 0) {
-        throw new IllegalArgumentException("Error: No input argument provided.");
+
+    /**
+     * The main method that takes an input argument, converts it to a decimal number,
+     * and prints its hexadecimal representation.
+     * @param args Command line arguments, expecting a single integer.
+     */
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Error: No input argument provided.");
+        }
+
+        int decimalNumber;
+        try {
+            decimalNumber = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Error: The provided input is not a valid integer.");
+        }
+
+        System.out.println("Converting the Decimal Value " + decimalNumber + " to Hex...");
+        String hexResult = convertToHex(decimalNumber);
+        System.out.println("Hexadecimal representation is: " + hexResult);
     }
- 
-    int Arg1;
-    try {
-        Arg1 = Integer.parseInt(args[0]);
-    } catch (NumberFormatException e) {
-        throw new NumberFormatException("Error: The provided input is not a valid integer.");
-    }
- 
-    System.out.println("Converting the Decimal Value " + Arg1 + " to Hex...");
-    String hexResult = convertToHex(Arg1);
-    System.out.println("Hexadecimal representation is: " + hexResult);
 }
- 
-}
+
--- TestRunner.java
+++ TestRunner.java
@@ -1,20 +1,22 @@
 import org.junit.runner.JUnitCore;
 import org.junit.runner.Result;
 import org.junit.runner.notification.Failure;
- 
+
 public class TestRunner {
     public static void main(String[] args) {
+        // Run the test class using JUnitCore
         Result result = JUnitCore.runClasses(Dec2HexTest.class);
-        int fails = 0;
+
+        // Iterate through and print details of any failed tests
         for (Failure failure : result.getFailures()) {
             System.out.println(failure.toString());
-            fails++;
         }
-        if (fails > 1) {
-            System.out.println(result.wasSuccessful());
-            System.exit(1);
+
+        // Print the overall test result
+        if (result.wasSuccessful()) {
+            System.out.println("All tests passed successfully.");
+        } else {
+            System.out.println("Some tests failed.");
         }
-        System.out.println("All tests passed.");
-        System.exit(0);
     }
 }
