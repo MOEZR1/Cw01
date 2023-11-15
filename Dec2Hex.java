ecc3461465b72ffb3b93b5869aa235074ec95165
MOEZR1 <mohammedalbahly206@gmail.com>
2023-11-15 22:48:58 +0000

    updated all files

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,5 +1,5 @@
 public class Dec2Hex {
-
+//MOhammed
     private static final char[] HEX_CHARS = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
 
     public static void main(String[] args) {
