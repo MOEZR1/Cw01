039a3b6a2f322b97802702b2e86eed7659187b7f
MOEZR1 <mohammedalbahly206@gmail.com>
2023-11-15 23:28:53 +0000

    updated 2

--- Dec2Hex.java
+++ Dec2Hex.java
@@ -1,26 +1,42 @@
 public class Dec2Hex {
-    static int Arg1;
 
-    public static void main(String args[]) {
-        // Parse the first argument as an integer
-        Arg1 = Integer.parseInt(args[0]);
-
-        // Character array representing hexadecimal digits
-        char ch[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
-        
-        int rem, num;
-        num = Arg1; // Assign input number to 'num'
-        String hexadecimal = ""; // Initialize hexadecimal string
-
-        System.out.println("Converting the Decimal Value " + num + " to Hex...");
-
-        // Conversion process using modulo and division
-        while (num != 0) {
-            rem = num % 16; // Get remainder
-            hexadecimal = ch[rem] + hexadecimal; // Convert to hex digit and append
-            num = num / 16; // Reduce number
+    /**
+     * Converts a decimal number to its hexadecimal representation.
+     * @param decimalNumber The integer to convert to a hexadecimal string.
+     * @return A string representing the hexadecimal value of the input integer.
+     */
+    public static String convertToHex(int decimalNumber) {
+        // Hexadecimal characters
+        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+        // StringBuilder is used for efficient string concatenation
+        StringBuilder hexadecimal = new StringBuilder();
+        // Loop to process the number until division result is 0
+        while (decimalNumber > 0) {
+            int rem = decimalNumber % 16; // Remainder of division by 16
+            hexadecimal.insert(0, ch[rem]); // Prepend the hex character
+            decimalNumber /= 16; // Divide by 16 for next iteration
         }
-
-        System.out.println("Hexadecimal representation is: " + hexadecimal);
+        return hexadecimal.toString();
     }
+
+    public static void main(String[] args) {
+        // Check if an argument is provided
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Error: No input argument provided.");
+        }
+ 
+        int Arg1;
+        // Try to parse the argument as an integer
+        try {
+            Arg1 = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            throw new NumberFormatException("Error: The provided input '" + args[0] + "' is not a valid integer.");
+        }
+ 
+        // Output the conversion process
+        System.out.println("Converting the Decimal Value " + Arg1 + " to Hex...");
+        String hexResult = convertToHex(Arg1);
+        System.out.println("Hexadecimal representation is: " + hexResult);
+    }
+ 
 }
--- Dec2HexTest.java
+++ Dec2HexTest.java
@@ -1,14 +1,49 @@
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class Dec2HexTest {
 
+    /**
+     * Tests the convertToHex method with a valid integer input.
+     * The expected output for decimal 255 is hexadecimal "FF".
+     */
     @Test
-    public void testConversion() {
-        String[] args = {"15"};
-        Dec2Hex.main(args); // Execute with a test value
-        // The output should be verified manually, or you can implement a method in Dec2Hex to return the hex value and test it here.
+    public void testValidIntegerInput() {
+        String result = Dec2Hex.convertToHex(255);
+        assertEquals("FF", result);
     }
 
+    /**
+     * Tests the main method with no arguments.
+     * Expects an IllegalArgumentException due to no arguments provided.
+     */
+    @Test(expected = IllegalArgumentException.class)
+    public void testNoInputArgument() {
+        // Redirects the standard error to prevent the exception message from appearing in the console during tests.
+        System.setErr(new java.io.PrintStream(new java.io.OutputStream() {
+            @Override
+            public void write(int b) {
+                // NO-OP
+            }
+        }));
 
+        Dec2Hex.main(new String[]{});
+    }
+
+    /**
+     * Tests the main method with a non-integer input.
+     * Expects a NumberFormatException due to invalid number format.
+     */
+    @Test(expected = NumberFormatException.class)
+    public void testNonIntegerInput() {
+        // Redirects the standard error to prevent the exception message from appearing in the console during tests.
+        System.setErr(new java.io.PrintStream(new java.io.OutputStream() {
+            @Override
+            public void write(int b) {
+                // NO-OP
+            }
+        }));
+
+        Dec2Hex.main(new String[]{"abc"});
+    }
 }
